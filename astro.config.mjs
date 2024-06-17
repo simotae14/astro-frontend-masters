37dd1cc1fc8240dcef9526b89d4c038978f2535a
Simone Taeggi <startae14@gmail.com>
2024-06-18 00:26:35 +0200

    feat:site ready to deploy in Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-frontend--masters.netlify.app',
-	output: 'hybrid',
-	experimental: {
-		hybridOutput: true,
-	},
-	integrations: [react(), solidJs()],
+  site: 'https://astro-frontend--masters.netlify.app',
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true
+  },
+  integrations: [react(), solidJs()],
+  adapter: netlify()
 });
