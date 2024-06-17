c46ec87ea5e3f7d4dc4f3ee4ce149df78130a61a
Simone Taeggi <startae14@gmail.com>
2024-06-18 00:35:55 +0200

    fix: Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import solidJs from '@astrojs/solid-js';
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-frontend--masters.netlify.app',
-  output: 'hybrid',
-  experimental: {
-    hybridOutput: true
-  },
-  integrations: [react(), solidJs()],
-  adapter: netlify()
+	site: 'https://astro-frontend--masters.netlify.app',
+	output: 'hybrid',
+	integrations: [react(), solidJs()],
+	adapter: netlify(),
 });
