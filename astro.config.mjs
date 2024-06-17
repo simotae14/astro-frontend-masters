ca6268647e4fb352cd1bbd8a26523a25e2e91e59
Simone Taeggi <startae14@gmail.com>
2024-06-17 23:08:19 +0200

    Enabling SSR & Forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import solidJs from '@astrojs/solid-js';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://astro-frontend--masters.netlify.app',
+	output: 'hybrid',
+	experimental: {
+		hybridOutput: true,
+	},
 	integrations: [react(), solidJs()],
 });
